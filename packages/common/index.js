4ba7e25b05b188d25863f062a1fc6ba8b06d008a
Manuel Martin <manuel.martin@gmail.com>
2020-09-26 23:09:58 +0200

    feat(common): add box-sizing style

--- packages/common/index.js
+++ packages/common/index.js
@@ -1,1 +1,5 @@
+// *** providers **********************************************************************************
 export { DataProvider } from './src/providers/DataProvider.js';
+
+// *** styles *************************************************************************************
+export { boxSizingStyle } from './src/styles/boxSizing.style.js';
